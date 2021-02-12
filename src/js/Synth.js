49a613fcf743f4d3182a29071cda1da7eb3bd727
petethomasmusique <petethomasmusique@gmail.com>
2021-02-12 21:54:25 +0000

    backup

--- src/js/Synth.js
+++ src/js/Synth.js
@@ -6,11 +6,14 @@ class Synth {
     }
     init() { 
         this.gain = new Tone.Gain(0.25).connect(output)
-        this.synth = new Tone.FMSynth({oscillator: {type: 'sine'}}).connect(this.gain)
+        this.synth = new Tone.FMSynth({oscillator: {type: 'sine'}, modulationIndex: 2}).connect(this.gain)
     }
     cleanUp() {
-        this.gain.dispose()
-        this.synth.dispose()
+        this.synth.triggerRelease()
+        setTimeout(() => {
+            this.synth.dispose("+1")
+            this.gain.dispose("+1")
+        }, 2000)
     }
 }
 
