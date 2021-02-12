062b050c631ac6d29496ec687bc62da7e6ac3b2d
petethomasmusique <petethomasmusique@gmail.com>
2021-02-12 17:04:45 +0000

    start audio context

--- src/index.js
+++ src/index.js
@@ -13,7 +13,14 @@ import * as Tone from 'tone'
 import './styles/index.scss'
 
 let board = new Board(["d2", "a2", "d3", "a3", "c4", "e4", "f4", "g4", "a4", "c5", "e5"])
+let audioIsRunning = false
 
+document.addEventListener('keydown', async () => {
+    if(audioIsRunning) return
+	await Tone.start()
+    audioIsRunning = true
+	console.log('audio is ready')
+})
 document.addEventListener('keydown', function(e) {
     if(['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'].includes(e.key)) board.setCursor(e.key)
     if(['n', 's', 'e', 'w'].includes(e.key)) board.createBlock(e.key)
